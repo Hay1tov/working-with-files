cd40c1b5169caf1da34c26c4dd129e252d4915e1
Hay1tov <eldorh35@gmail.com>
2024-12-19 14:42:44 +0500

    Bajarildi

--- 1-misol.c
+++ 1-misol.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+int main() {
+    FILE *input_file = fopen("input.txt", "r");
+    FILE *output_file = fopen("output.txt", "w");
+
+    int number; 
+    double s = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        fscanf(input_file, "%d", &number);
+        s += number;
+    }
+
+    fprintf(output_file, " Natija = %lf", s/10);
+
+    fclose(input_file);
+    fclose(output_file);
+}
